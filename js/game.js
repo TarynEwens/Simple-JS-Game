c1984544b098b2aae60ff390904a12fd5eff2701
tarynewens <taryn@taryn.codes>
2021-07-07 13:57:47 +1000

    taryn's commit

--- js/game.js
+++ js/game.js
@@ -9,15 +9,17 @@ document.body.appendChild(canvas);
 var bgReady = false;
 var bgImage = new Image();
 bgImage.onload = function () {
-	bgReady = true;
+  bgReady = true;
 };
 bgImage.src = "images/background-poly.png";
+
+// THIS IS A RANDOM COMMENT BY TARYN
 
 // Hero image
 var petReady = false;
 var petImage = new Image();
 petImage.onload = function () {
-	petReady = true;
+  petReady = true;
 };
 petImage.src = "images/hero copy.png";
 
@@ -25,13 +27,13 @@ petImage.src = "images/hero copy.png";
 var cheeseReady = false;
 var cheeseImage = new Image();
 cheeseImage.onload = function () {
-	cheeseReady = true;
+  cheeseReady = true;
 };
 cheeseImage.src = "images/cheese.png";
 
 // Game objects
 var pet = {
-	speed: 256 // movement in pixels per second
+  speed: 256, // movement in pixels per second
 };
 var cheese = {};
 var cheesesCaught = 0;
@@ -39,90 +41,106 @@ var cheesesCaught = 0;
 // Handle keyboard controls
 var keysDown = {};
 
-addEventListener("keydown", function (e) {
-	keysDown[e.keyCode] = true;
-}, false);
+addEventListener(
+  "keydown",
+  function (e) {
+    keysDown[e.keyCode] = true;
+  },
+  false
+);
 
-addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
-}, false);
+addEventListener(
+  "keyup",
+  function (e) {
+    delete keysDown[e.keyCode];
+  },
+  false
+);
 
 // Reset the game when the player catches a cheese
 var reset = function () {
-	pet.x = canvas.width / 2;
-	pet.y = canvas.height / 2;
+  pet.x = canvas.width / 2;
+  pet.y = canvas.height / 2;
 
-	// Throw the cheese somewhere on the screen randomly
-	cheese.x = 32 + (Math.random() * (canvas.width - 64));
-	cheese.y = 32 + (Math.random() * (canvas.height - 64));
+  // Throw the cheese somewhere on the screen randomly
+  cheese.x = 32 + Math.random() * (canvas.width - 64);
+  cheese.y = 32 + Math.random() * (canvas.height - 64);
 };
 
 // Update game objects
 var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		pet.y -= pet.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		pet.y += pet.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		pet.x -= pet.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		pet.x += pet.speed * modifier;
-	}
+  if (38 in keysDown) {
+    // Player holding up
+    pet.y -= pet.speed * modifier;
+  }
+  if (40 in keysDown) {
+    // Player holding down
+    pet.y += pet.speed * modifier;
+  }
+  if (37 in keysDown) {
+    // Player holding left
+    pet.x -= pet.speed * modifier;
+  }
+  if (39 in keysDown) {
+    // Player holding right
+    pet.x += pet.speed * modifier;
+  }
 
-	// Are they touching?
-	if (
-		pet.x <= (cheese.x + 32)
-		&& cheese.x <= (pet.x + 32)
-		&& pet.y <= (cheese.y + 32)
-		&& cheese.y <= (pet.y + 32)
-	) {
-		++cheesesCaught;
-		reset();
-	}
+  // Are they touching?
+  if (
+    pet.x <= cheese.x + 32 &&
+    cheese.x <= pet.x + 32 &&
+    pet.y <= cheese.y + 32 &&
+    cheese.y <= pet.y + 32
+  ) {
+    ++cheesesCaught;
+    reset();
+  }
 };
 
 // Draw everything
 var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
-	}
+  if (bgReady) {
+    ctx.drawImage(bgImage, 0, 0);
+  }
 
-	if (petReady) {
-		ctx.drawImage(petImage, pet.x, pet.y);
-	}
+  if (petReady) {
+    ctx.drawImage(petImage, pet.x, pet.y);
+  }
 
-	if (cheeseReady) {
-		ctx.drawImage(cheeseImage, cheese.x, cheese.y);
-	}
+  if (cheeseReady) {
+    ctx.drawImage(cheeseImage, cheese.x, cheese.y);
+  }
 
-	// Score
-	ctx.fillStyle = "rgb(50, 50, 50)";
-	ctx.font = "24px Helvetica";
-	ctx.textAlign = "left";
-	ctx.textBaseline = "top";
-	ctx.fillText("Pieces of cheese: " + cheesesCaught, 32, 32);
+  // Score
+  ctx.fillStyle = "rgb(50, 50, 50)";
+  ctx.font = "24px Helvetica";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText("Pieces of cheese: " + cheesesCaught, 32, 32);
 };
 
 // The main game loop
 var main = function () {
-	var now = Date.now();
-	var delta = now - then;
+  var now = Date.now();
+  var delta = now - then;
 
-	update(delta / 1000);
-	render();
+  update(delta / 1000);
+  render();
 
-	then = now;
+  then = now;
 
-	// Request to do this again ASAP
-	requestAnimationFrame(main);
+  // Request to do this again ASAP
+  requestAnimationFrame(main);
 };
 
 // Cross-browser support for requestAnimationFrame
 var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+requestAnimationFrame =
+  w.requestAnimationFrame ||
+  w.webkitRequestAnimationFrame ||
+  w.msRequestAnimationFrame ||
+  w.mozRequestAnimationFrame;
 
 // Let's play this game!
 var then = Date.now();
